f6989e4c35c04ff3f81fe6444a220be7e465c16a
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-13 18:15:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
     WIN_COMBINATIONS.detect do |win_combination|
-      board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && && position_taken?(board, win_combination[0])
+      board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken?(board, win_combination[0])
     #  win_index_1 = win_combination[0]
     #  win_index_2 = win_combination[1]
     #  win_index_3 = win_combination[2]
