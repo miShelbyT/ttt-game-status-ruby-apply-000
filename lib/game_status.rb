5b2645d30a8bcb0dd4f692fd48a00ab6568d6ee7
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-13 18:12:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,21 +17,22 @@ WIN_COMBINATIONS = [
 
 def won?(board)
     WIN_COMBINATIONS.detect do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
+      board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && && position_taken?(board, win_combination[0])
+    #  win_index_1 = win_combination[0]
+    #  win_index_2 = win_combination[1]
+    #  win_index_3 = win_combination[2]
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+    #  position_1 = board[win_index_1]
+    #  position_2 = board[win_index_2]
+    #  position_3 = board[win_index_3]
 
-      if position_1 == ["X"] && position_2 == ["X"] && position_3 == ["X"] && position_taken?(board, index)
-        return win_combination
-      elsif position_1 == ["O"] && position_2 == ["O"] && position_3 == ["O"] && position_taken?(board, index)
-        return win_combination
-      else
-        return false
-       end
+    #  if position_1 == ["X"] && position_2 == ["X"] && position_3 == ["X"] && position_taken?(board, index)
+    #    return win_combination
+    #  elsif position_1 == ["O"] && position_2 == ["O"] && position_3 == ["O"] && position_taken?(board, index)
+    #    return win_combination
+    #  else
+    #    return false
+    #   end
     end
   end
 
